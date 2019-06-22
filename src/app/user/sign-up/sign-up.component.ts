2d6e4df83c7912a5b0b15ca909107eeb79e569ea
MKA1500 <maciejkabsch@gmail.com>
2019-06-22 21:30:36 +0200

    registration form template

--- src/app/user/sign-up/sign-up.component.ts
+++ src/app/user/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../../shared/user.model';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  user: User;
+  submitted = false;
+  @ViewChild('userRegistrationForm') signupForm: NgForm;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log(this.signupForm.value);
+
+    // this.submitted = true;
+
+    // this.user.username = this.signupForm.value.userData.username;
+    // this.user.email = this.signupForm.value.userData.email;
+    // this.user.gender = this.signupForm.value.userData.gender;
+    // this.user.secretQuestion = this.signupForm.value.secret;
+    // this.user.answer = this.signupForm.value.questionAnswer;
+
+    // this.signupForm.reset();
+  }
 }
